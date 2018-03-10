a3444e95325e9568b5f3de72690f776e6725c4ba
test <asdfg@mail.com>
2018-03-10 09:12:28 +0700

    Variable

---
+++ VariableAndConstant.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//การประกาศตัวแปร แบบแก้ไขค่าได้
+var nameString = "napat"
+
+//ลองแก้ไขค่าของตัวแปรname
+nameString = "Doremon"//สามารถแก้ไขได้ค่าได้ ถ้าเป็นvar
+
+
+
+
