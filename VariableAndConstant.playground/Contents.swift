1a024d381c48f408537e9d7ec3214bd54758cfc2
test <asdfg@mail.com>
2018-03-10 09:23:36 +0700

    Type Data

--- VariableAndConstant.playground/Contents.swift
+++ VariableAndConstant.playground/Contents.swift
@@ -3,7 +3,17 @@
 import UIKit
 
 //การประกาศตัวแปร แบบแก้ไขค่าได้
+//ประกาศแบบไม่กำหนดชนิดข้อมูล แต่จะกำหนดจากvalue
 var nameString = "napat"
+
+//นี้คือการประกาศตัวแปรแบบกำหนด ชนิดของข้อมูล
+var surnameString: String = "changlao"
+var numberAInt: Int = 123
+var numberDouble: Double = 3.14
+var statusBool: Bool = true
+
+
+
 
 //ลองแก้ไขค่าของตัวแปรname
 nameString = "Doremon"//สามารถแก้ไขได้ค่าได้ ถ้าเป็นvar
